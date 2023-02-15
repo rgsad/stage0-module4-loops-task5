90f0f663b5e81973420079bb628dc58ca67cb932
rgsad <rsad3262@gmail.com>
2023-02-16 01:01:34 +0300

    solved the "Cross" task & "Square" task

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i != (sideLength - 1) / 2 && j != (sideLength - 1) / 2) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("8");
+                }
+            }
+            System.out.println();
+        }
     }
 }
