db6c4776b47e1deae7ba32ea41bab880d5822921
rgsad <rsad3262@gmail.com>
2023-02-18 01:42:07 +0300

    solved the "Hourglass" task

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i < height / 2) {
+                    if (j < i || j >= height - i) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("8");
+                    }
+                }
+                if (i >= height / 2) {
+                    if (j < height - 1 - i || j > i) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("8");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
